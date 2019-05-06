f3d01b69487b109f3bba642a9f54271bcb55f449
AddChan <is528629@gmail.com>
2019-05-07 08:48:30 +0900

    Initial Commit

---
+++ app/src/main/java/add/chan/sichan/androidkotlinstudy/MainActivity.kt
@@ -0,0 +1,14 @@
+package add.chan.sichan.androidkotlinstudy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //TODO Begin studying Kotlin1
+    }
+}
