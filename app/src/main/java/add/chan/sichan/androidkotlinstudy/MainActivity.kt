9c83ded800ea2d032e316d5e5c15c6d1f306ccf9
AddChan <is528629@gmail.com>
2019-05-08 10:15:12 +0900

    Tutorial1 Commit

--- app/src/main/java/add/chan/sichan/androidkotlinstudy/MainActivity.kt
+++ app/src/main/java/add/chan/sichan/androidkotlinstudy/MainActivity.kt
@@ -1,7 +1,9 @@
 package add.chan.sichan.androidkotlinstudy
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO Begin studying Kotlin1
+        btn_kotlin_tutorial1.setOnClickListener {
+            val intentTutorial = Intent(this,TutorialActivity::class.java)
+            startActivity(intentTutorial)
+        }
+
+        // 단축키
+        // Ctrl+E 화면전환 단축키
+
     }
 }
