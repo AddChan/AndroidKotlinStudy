a96c009ecc0d4382671ae7120fa7e278d6f0f2ba
AddChan <is528629@gmail.com>
2019-05-10 15:31:49 +0900

    Tutorial3 Commit

--- app/src/main/java/add/chan/sichan/androidkotlinstudy/Tutorial3_StopWatch/StopWatchActivity.kt
+++ app/src/main/java/add/chan/sichan/androidkotlinstudy/Tutorial3_StopWatch/StopWatchActivity.kt
@@ -3,12 +3,92 @@ package add.chan.sichan.androidkotlinstudy.Tutorial3_StopWatch
 import add.chan.sichan.androidkotlinstudy.R
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_stop_watch.*
+import java.util.*
+import kotlin.concurrent.timer
 
+/**
+ *
+timer(period = 1000){
+// 수행할 동작
+runOnUiThread {
+// UI 조작
+}
+}
+ */
 class StopWatchActivity : AppCompatActivity() {
+    private var time = 0
+    private var isRunning = false
+    private var timerTask: Timer? = null
+    private var lap = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stop_watch)
 
+        fab.setOnClickListener {
+            isRunning = !isRunning
+
+            if (isRunning) {
+                start()
+            } else {
+                pause()
+            }
+        }
+
+        lapButton.setOnClickListener {
+            recordLapTime()
+        }
+
+        resetFab.setOnClickListener {
+            reset()
+        }
+    }
+
+    private fun reset() {
+        timerTask?.cancel()
+
+        // 모든 변수 초기화
+        time = 0
+        isRunning = false
+        fab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        secTextView.text = "0"
+        milliTextView.text = "00"
+
+        // 모든 랩타임 제거
+        lapLayout.removeAllViews()
+        lap = 1
+    }
+
+    private fun recordLapTime() {
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.text = "$lap LAB : ${lapTime / 100}.${lapTime % 100}"
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f) // textsize 조정
+
+        lapLayout.addView(textView, 0)
+        lap++
+    }
+
+    private fun start() {
+        fab.setImageResource(R.drawable.ic_pause_black_24dp)
+
+        timerTask = timer(period = 10) {
+            time++
+            val sec = time / 100
+            val milli = time % 100
+            runOnUiThread {
+                secTextView.text = "$sec"
+                milliTextView.text = "$milli"
+            }
+
+        }
+    }
+
+    private fun pause() {
+        fab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        timerTask?.cancel()
     }
 }
